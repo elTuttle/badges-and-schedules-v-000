07536bbd3c567b0cbcb475bb112144e5558c0875
John Tuttle <thejwt@gmail.com>
2017-09-25 23:54:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def batch_badge_creator(name_array)
+
+  name_array.each_with_index do |name, index|
+    name_array[index] = "Hello, #{name}! You'll be assigned to room #{name_array[index] + 1}"
+  end
+  
+end
