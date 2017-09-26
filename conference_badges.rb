8c2f2f17dd7fd5b8f98cdac0d1b97af06486c303
John Tuttle <thejwt@gmail.com>
2017-09-26 00:27:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,8 @@ end
 def printer (attendees)
   batch_badge_creator(attendees).each do |i|
     puts i
-    assign_rooms(attendees).each do |i|
-      puts i
-    end
+  end
+  assign_rooms(attendees).each do |i|
+    puts i
   end
 end
