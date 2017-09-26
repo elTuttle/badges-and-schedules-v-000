770b132d8dc86b99877a2521e3f8769cafe608cd
John Tuttle <thejwt@gmail.com>
2017-09-26 00:03:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,9 @@
 def batch_badge_creator(attendees)
-
+attendees_list = []
   attendees.each do |name|
+    attendees_list.push("Hello, my name is #{name}.")
   end
+  attendees_list
 end
 
 def badge_maker(name)
